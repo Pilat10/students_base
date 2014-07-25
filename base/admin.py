514c5f20b9dfb440516e3066760d7c4b135ef2b5
Anton <pilat1091@gmail.com>
2014-07-25 22:53:47 +0300

    chenge models and add models in admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from base.models import Student
+from base.models import Group
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Group)
--- base/models.py
+++ base/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 
 
+class Group(models.Model):
+    """
+
+    """
+    name = models.CharField(max_length=255)
+    headman = models.ForeignKey("Student", related_name='+')
+
+
 class Student(models.Model):
     """
 
     """
     fio = models.CharField(max_length=255)
     birthday = models.DateField()
-    number_student_cart = models.ImageField()
+    number_student_cart = models.IntegerField()
     group = models.ForeignKey("Group")
-
-
-class Group(models.Model):
-    """
-
-    """
-    name = models.CharField(max_length=255)
-    headman = models.ForeignKey("Student")
