1d21c8f44a218ff24fb919a04e1abbba3014a772
Anton <pilat1091@gmail.com>
2014-09-14 17:18:57 +0300

    a new files

--- base/views.py
+++ base/views.py
@@ -1,13 +1,53 @@
 from django.shortcuts import render
-from base.models import Group
-from django.views.generic import ListView
+from base.models import Group, Student
+from django.views.generic import ListView, CreateView
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
 
 class GroupList(ListView):
     """
-    Out group list.
+    Out groups list.
     """
     model = Group
     template_name = "base/group_list.html"
+    context_object_name = "group_list"
+
+    def get_context_data(self, **kwargs):
+        context = super(GroupList, self).get_context_data(**kwargs)
+        context['title'] = "Group list"
+        return context
+
+
+class StudentList(ListView):
+    """
+    Out students list
+    """
+    template_name = "base/student_list.html"
+    context_object_name = "student_list"
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentList, self).get_context_data(**kwargs)
+        context['title'] = 'Stident list group %s' % self.kwargs['group_id']
+        return context
+
+    def get_queryset(self):
+        return Student.objects.filter(group__pk=self.kwargs['group_id'])
+
+
+class GroupAdd(CreateView):
+    """
+    add group
+    """
+    model = Group
+    fields = ['name', 'headman']
+    template_name = "base/group_add.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(GroupAdd, self).get_context_data(**kwargs)
+        context['title'] = 'Group add'
+        return context
+
+    def get_success_url(self):
+        return reverse('group_list')
--- students_base/urls.py
+++ students_base/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from base.views import GroupList
+from base.views import GroupList, StudentList, GroupAdd
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,5 +13,9 @@ urlpatterns = patterns('',
 
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^groups/', GroupList.as_view(), name='group_list')
+    url(r'^groups/$', GroupList.as_view(), name='group_list'),
+    url(r'^groups/(?P<group_id>\d+)/$', StudentList.as_view(),
+        name='student_list'),
+    url(r'^groups/add/$', GroupAdd.as_view(), name='group_add'),
+
 )
