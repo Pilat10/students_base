e3307319e46be5e3c96458c0ae704e34c4d9b0a1
Anton <pilat1091@gmail.com>
2014-10-08 07:14:18 +0300

    chenge signals

--- base/signal_models.py
+++ base/signal_models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-ADD = 1
-CHANGE = 2
-DELETE = 3
-
 
 class LogEntry(models.Model):
     """
@@ -22,3 +18,8 @@ class LogEntry(models.Model):
     def __unicode__(self):
         return self.get_action_flag_display() + " '{}'".format(
             self.object_descript)
+
+    def get_action_status(self, status):
+        for act in self.ACTION_STATUS:
+            if act[1] == status:
+                return act[0]
--- base/signals.py
+++ base/signals.py
@@ -1,14 +1,13 @@
 __author__ = 'pilat10'
-from base.signal_models import LogEntry, ADD, CHANGE, DELETE
+from base.signal_models import LogEntry
 
 
 def obj_save(sender, **kwargs):
     obj = kwargs['instance']
     save_model = LogEntry()
-    flag = CHANGE
-    #flag = LogEntry.ACTION_STATUS['ADD']
+    flag = save_model.get_action_status('change')
     if kwargs['created']:
-        flag = ADD
+        flag = save_model.get_action_status('add')
     save_model.action_flag = flag
     save_model.object_id = obj.pk
     save_model.object_descript = obj
@@ -18,7 +17,7 @@ def obj_save(sender, **kwargs):
 def obj_delete(sender, **kwargs):
     obj = kwargs['instance']
     save_model = LogEntry()
-    flag = DELETE
+    flag = save_model.get_action_status('delete')
     save_model.action_flag = flag
     save_model.object_id = obj.pk
     save_model.object_descript = obj
