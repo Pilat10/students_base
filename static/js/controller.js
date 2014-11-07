a0c9d2c8f2a2d99cb61e117f4d6ed3dbbb4171cc
Pilat10 <Pilat1091@gmail.com>
2014-11-07 17:01:38 +0200

    angular

---
+++ static/js/controller.js
@@ -0,0 +1,11 @@
+function PhoneListCtrl($scope) {
+    $scope.mynamb = "dddddddd"
+    $scope.phones = [
+    {"name": "Nexus S",
+    "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+    "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM™",
+    "snippet": "The Next, Next Generation tablet."}
+    ];
+}
