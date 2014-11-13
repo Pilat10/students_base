78126a28490cfbc11146b279c383390e42a32b6d
Anton <pilat1091@gmail.com>
2014-11-13 12:54:02 +0200

    chenge settings

--- students_base/settings.py
+++ students_base/settings.py
@@ -93,8 +93,9 @@ AUTHENTICATION_BACKENDS = (
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
