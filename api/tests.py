deacef3e6ac106344bd5b31c6b52bd8e7989342c
Pilat10 <Pilat1091@gmail.com>
2014-12-10 17:38:14 +0200

    add tests for group, change name field in GroupSerializer, change data in test_data.json

--- api/tests.py
+++ api/tests.py
@@ -90,7 +90,7 @@ class DepartmentTestCase(APITestCase):
 
     def test_is_fixtures_load(self):
         """
-        test load fixture with model department and auth models
+        test load fixture with model department, group, student and auth models
         """
         self.assertTrue(Department.objects.all().exists())
         self.assertTrue(Group.objects.all().exists())
@@ -262,24 +262,168 @@ class GroupTestCase(APITestCase):
     """
     test api group get, post, put, delete
     """
-    fixtures = ['initial_data', 'auth']
+    fixtures = ['test_data', 'auth']
 
     def setUp(self):
         self.client.login(username='admin', password='admin')
+        self.group_list_url = reverse("api:group-list")
+        self.expected_response_keys = {
+            "id",
+            "name",
+            "department",
+            "count_student",
+            "headman",
+            "headman_name"
+        }
 
-    def test_add_group_success(self):
+    def test_is_fixtures_load(self):
         """
-        test add group
+        test load fixture with model department, group, student and auth models
+        """
+        self.assertTrue(Department.objects.all().exists())
+        self.assertTrue(Group.objects.all().exists())
+        self.assertTrue(Student.objects.all().exists())
+        self.assertTrue(User.objects.all().exists())
+        self.assertTrue(Group_auth.objects.all().exists())
+        self.assertTrue(Permission.objects.all().exists())
+
+    def test_get_group_list_success(self):
+        """
+        test api get group list success
+        """
+        count_group = Group.objects.count()
+        self.assertEqual(5, count_group)
+        get = self.client.get(self.group_list_url)
+        self.assertEqual(get.status_code, HTTP_200_OK)
+        self.assertEqual(get.data.get("status"), SUCCESS_STATUS)
+        self.assertEqual(len(get.data.get("data")), count_group)
+
+    def test_post_group_success(self):
+        """
+        test api post group success
         """
         group = {
-            "name": "group 5",
+            "name": "test group 1",
+            "department": 1,
         }
-        #add = self.client.post('/api/v1/group/', group)
-        #self.assertEqual(add.status_code, HTTP_201_CREATED)
-        #get = self.client.get('/api/v1/group/')
-        #self.assertEqual(get.status_code, HTTP_200_OK)
-        #gets = get.data.get("data")
-        #self.assertEqual(gets[0].get("name"), 2)
-        #gg = Group.objects.get(pk=1)
-        #self.assertEqual(gg.name, 2)
-        #self.assertTrue(Group.objects.all().exists())
+        count_group = Group.objects.count()
+        post = self.client.post(self.group_list_url, group)
+        self.assertEqual(post.status_code, HTTP_201_CREATED)
+        self.assertEqual(post.data.get("status"), SUCCESS_STATUS)
+        self.assertEqual(
+            set(post.data.get("data").keys()), self.expected_response_keys)
+        self.assertEqual(Group.objects.count(), count_group + 1)
+
+    def test_post_group_bad_data(self):
+        """
+        test api post group bad data
+        """
+        group = {
+            "name": "test group 1",
+            "department": Department.objects.last().pk + 1,
+        }
+        count_group = Group.objects.count()
+        add = self.client.post(self.group_list_url, group)
+        self.assertEqual(add.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(add.data.get("status"), FAIL_STATUS)
+        self.assertEqual(Group.objects.count(), count_group)
+        self.assertEqual(
+            set(add.data.get("data").keys()), {"department", })
+
+    def test_get_group_detail_success(self):
+        """
+        test api get group detail success
+        """
+        group = Group.objects.first()
+        group_url = reverse(
+            'api:group-detail', kwargs={"pk": group.id})
+        get_detail = self.client.get(group_url)
+        self.assertEqual(get_detail.status_code, HTTP_200_OK)
+        self.assertEqual(get_detail.data.get("status"), SUCCESS_STATUS)
+        self.assertEqual(
+            set(get_detail.data.get("data").keys()), self.expected_response_keys)
+
+    def test_put_group_fail(self):
+        """
+        test api put group fail
+        """
+        group = Group.objects.last()
+        group_url = reverse(
+            'api:group-detail', kwargs={"pk": group.id+1})
+        get = self.client.get(group_url)
+        self.assertEqual(get.status_code, HTTP_404_NOT_FOUND)
+        self.assertEqual(get.data.get("status"), FAIL_STATUS)
+
+
+    def test_put_group_success(self):
+        """
+        test api put group success
+        """
+        group = Group.objects.first()
+        department = Department.objects.first()
+        if department == group.department:
+            department = Department.objects.last()
+        headman = Student.objects.filter(group=group.id).first()
+        if headman == group.headman:
+            headman = Student.objects.filter(group=group.id).last()
+
+        group_new = {
+            "id": group.id,
+            "name": group.name+"new",
+            "department": department.id,
+            "headman": headman.id,
+        }
+        group_url = reverse(
+            'api:group-detail', kwargs={"pk": group.id})
+        get = self.client.get(group_url)
+        self.assertEqual(get.status_code, HTTP_200_OK)
+        self.assertEqual(get.data.get("status"), SUCCESS_STATUS)
+        put = self.client.put(group_url, group_new)
+        self.assertEqual(put.status_code, HTTP_200_OK)
+        self.assertEqual(put.data.get("status"), SUCCESS_STATUS)
+        self.assertEqual(
+            set(put.data.get("data").keys()), self.expected_response_keys)
+        updated_group = Group.objects.get(pk=group.pk)
+        self.assertNotEqual(
+            group.name, updated_group.name)
+        if department != group.department:
+            self.assertNotEqual(group.department, updated_group.department)
+        if headman != group.headman:
+            self.assertNotEqual(group.headman, updated_group.headman)
+
+    def test_put_group_bad_data(self):
+        """
+        test api put group bad_data
+        """
+        group = Group.objects.first()
+        if group.headman is not None:
+            headman = group.headman.id
+        else:
+            headman = group.headman
+        group_new = {
+            "id": group.id,
+            "name": "",
+            "department": group.department.id,
+            "headman": headman
+        }
+        group_url = reverse(
+            'api:group-detail', kwargs={"pk": group.id})
+        put = self.client.put(group_url, group_new)
+        self.assertEqual(put.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(put.data.get("status"), FAIL_STATUS)
+        self.assertEqual(
+            set(put.data.get("data").keys()), {"name", })
+
+
+    def test_delete_group_success(self):
+        """
+        test api delete group success
+        """
+        count_group = Group.objects.count()
+        delete_group = Group.objects.last()
+        group_url = reverse(
+            'api:group-detail', kwargs={"pk": delete_group.id})
+        post = self.client.delete(group_url)
+        self.assertEqual(post.status_code, HTTP_204_NO_CONTENT)
+        self.assertEqual(post.data.get("status"), SUCCESS_STATUS)
+        self.assertEqual(Group.objects.count(), count_group-1)
